ce020a86bf4dd81c3711312e0d9f87c57a468e3d
Nabin Paudyal <nabinpaudyal2057@gmail.com>
2023-12-30 00:04:25 +0545

    boarding

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,18 @@
 import 'package:flutter/material.dart';
-import 'package:my_project/Navigation%20Drawer/nav_drawer.dart';
+import 'package:my_project/Modern%20Bottom%20Navigation/moder_bottom_naav_bar.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: MyNavDrawer(),
+      home: ModernBottomNavBar(),
     );
   }
 }
